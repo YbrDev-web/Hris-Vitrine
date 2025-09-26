a1a1f36ad9628c868f5906fdd0ca2027ad8aa41e
YbrDev-web <ybenromdhane2003@gmail.com>
2025-09-26 15:14:01 +0200

    modélisation de la page contact

---
+++ hris-vitrine/resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>HRIS PRO CONSULTING - Accueil</title>
+</head>
+<body>
+    <h1>Bienvenue sur HRIS PRO CONSULTING</h1>
+    <p>Votre partenaire SIRH & Workday.</p>
+    <a href="{{ route('contact.form') }}">Contactez-nous</a>
+</body>
+</html>
