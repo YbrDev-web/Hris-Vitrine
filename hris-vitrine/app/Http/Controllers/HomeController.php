0b9e6f994a032e1b2d12cf2262778be99f7b16f8
YbrDev-web <ybenromdhane2003@gmail.com>
2025-09-26 15:12:01 +0200

    ajout fonctions dans le controller

--- hris-vitrine/app/Http/Controllers/HomeController.php
+++ hris-vitrine/app/Http/Controllers/HomeController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    // Page du formulaire de contact
+    public function contactForm()
+    {
+        return view('contact');
+    }
+
+    // Soumission du formulaire (on le développera à l'étape 4)
+    public function submitContact(Request $request)
+    {
+        // Temporaire pour test
+        return "Formulaire reçu : " . $request->input('name');
+    }
 }
