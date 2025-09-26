0e0de21e4a214c99d1ecc1c8da4e454aa6829076
YbrDev-web <ybenromdhane2003@gmail.com>
2025-09-26 15:18:03 +0200

    page contact

--- hris-vitrine/resources/views/contact.blade.php
+++ hris-vitrine/resources/views/contact.blade.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact - HRIS PRO CONSULTING</title>
+</head>
+<body>
+    <h1>Formulaire de contact</h1>
+
+    <form method="POST" action="{{ route('contact.submit') }}">
+        @csrf
+        <label>Nom :</label>
+        <input type="text" name="name" required><br>
+
+        <label>Email :</label>
+        <input type="email" name="email" required><br>
+
+        <label>Message :</label>
+        <textarea name="message" required></textarea><br>
+
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
 
