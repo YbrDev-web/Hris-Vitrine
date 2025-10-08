256c23ade3f6c20ecc1c28122a4e3a968aa1f0f7
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-08 12:58:35 +0200

    Route services

--- hris-vitrine/routes/web.php
+++ hris-vitrine/routes/web.php
@@ -12,5 +12,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/contact', [HomeController::class, 'contactForm'])->name('contact.form');
 Route::post('/contact', [HomeController::class, 'submitContact'])->name('contact.submit');
 Route::get('/a-propos', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
+Route::get('/services', [App\Http\Controllers\ServiceController::class, 'index'])->name('services');
 
 
