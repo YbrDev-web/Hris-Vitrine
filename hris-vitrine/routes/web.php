526f95633b05513c333f9cb7cefc5193892312bd
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-16 13:44:47 +0200

    ajout routes

--- hris-vitrine/routes/web.php
+++ hris-vitrine/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KnowledgeController;
+
 
 Route::get('/', function () {
     return view('welcome');
