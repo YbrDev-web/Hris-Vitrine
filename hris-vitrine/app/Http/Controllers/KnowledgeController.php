1e6ab85796f8e3a0a29c2b2499ef020298d1ca57
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-16 13:37:41 +0200

    mise en forme de knowledge controller

--- hris-vitrine/app/Http/Controllers/KnowledgeController.php
+++ hris-vitrine/app/Http/Controllers/KnowledgeController.php
@@ -1,10 +1,27 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
+use App\Models\Knowledge;
 use Illuminate\Http\Request;
 
 class KnowledgeController extends Controller
 {
-    //
+    public function index()
+    {
+        $knowledges = Knowledge::all();
+        return view('knowledge', compact('knowledges'));
+    }
+
+    public function rate(Request $request, Knowledge $knowledge)
+    {
+        $value = (int) $request->input('value');
+        
+        // Calculer la nouvelle moyenne
+        $knowledge->rating_count += 1;
+        $knowledge->average_rating = (($knowledge->average_rating * ($knowledge->rating_count - 1)) + $value) / $knowledge->rating_count;
+        $knowledge->save();
+
+        return response()->json(['average' => round($knowledge->average_rating, 1)]);
+    }
 }
