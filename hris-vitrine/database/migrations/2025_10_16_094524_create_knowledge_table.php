f56f1d38312d867657aff1bf2b30abf620bdb094
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-16 11:58:00 +0200

    ajout de champ dans la table knowledge

--- hris-vitrine/database/migrations/2025_10_16_094524_create_knowledge_table.php
+++ hris-vitrine/database/migrations/2025_10_16_094524_create_knowledge_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('knowledge', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('rating')->default(0);
             $table->timestamps();
         });
     }
