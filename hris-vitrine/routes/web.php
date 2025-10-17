455c4992841109ec64b44575cc203dba7873bf29
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-17 15:12:16 +0200

    ajout lien et formulaire de connexion

--- hris-vitrine/routes/web.php
+++ hris-vitrine/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+
 
 
 
@@ -16,5 +18,7 @@ Route::post('/contact', [HomeController::class, 'submitContact'])->name('contact
 Route::get('/a-propos', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
 Route::get('/services', [App\Http\Controllers\ServiceController::class, 'index'])->name('services');
 Route::get('/Knowledge', [HomeController::class, 'knowledge'])->name('Knowledge');
+Route::get('/login', [LoginController::class, 'show'])->name('login');
+
 
 
