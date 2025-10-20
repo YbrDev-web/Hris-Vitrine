9b207599fd279ed58957e1acff6dc8457662983c
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-20 15:05:08 +0200

    controller RSS

--- hris-vitrine/app/Http/Controllers/RSSController.php
+++ hris-vitrine/app/Http/Controllers/RSSController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Article;
 
 class RSSController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::latest()->take(10)->get();
+
+        $rssFeed = view('rss.feed', compact('articles'));
+
+        return response($rssFeed, 200)
+            ->header('Content-Type', 'application/rss+xml');
+    }
 }
