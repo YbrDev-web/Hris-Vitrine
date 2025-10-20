d61bda5ae8d9ac966b078e934361af1c2f21c099
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-20 09:43:13 +0200

    knowledge controller

--- hris-vitrine/app/Http/Controllers/KnowledgeController.php
+++ hris-vitrine/app/Http/Controllers/KnowledgeController.php
@@ -1,27 +1,40 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use App\Models\Knowledge;
 use Illuminate\Http\Request;
+use Vedmant\FeedReader\Facades\FeedReader;
 
 class KnowledgeController extends Controller
 {
     public function index()
     {
-        $knowledges = Knowledge::all();
-        return view('knowledge', compact('knowledges'));
+        // ✅ Exemple : flux RSS Workday Community ou un blog RH
+        $feedUrl = 'https://blog.workday.com/en-us/rss.xml';
+
+        // ✅ Lecture du flux
+        $feed = FeedReader::read($feedUrl);
+
+        // ✅ Transformation des données
+        $articles = collect($feed->get_items())->map(function ($item) {
+            return [
+                'title' => $item->get_title(),
+                'link' => $item->get_link(),
+                'desc' => strip_tags($item->get_description()),
+                'img' => $this->extractImage($item->get_description()) ?? 'https://source.unsplash.com/random/800x400?business,office',
+                'date' => $item->get_date('Y-m-d'),
+            ];
+        });
+
+        return view('knowledge', compact('articles'));
     }
 
-    public function rate(Request $request, Knowledge $knowledge)
+    // 🔧 Fonction utilitaire pour extraire une image du flux
+    private function extractImage($html)
     {
-        $value = (int) $request->input('value');
-        
-        // Calculer la nouvelle moyenne
-        $knowledge->rating_count += 1;
-        $knowledge->average_rating = (($knowledge->average_rating * ($knowledge->rating_count - 1)) + $value) / $knowledge->rating_count;
-        $knowledge->save();
-
-        return response()->json(['average' => round($knowledge->average_rating, 1)]);
+        if (preg_match('/<img.*?src=["\'](.*?)["\']/', $html, $matches)) {
+            return $matches[1];
+        }
+        return null;
     }
 }
