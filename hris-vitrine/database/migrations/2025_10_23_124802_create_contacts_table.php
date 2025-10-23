509a60ec30810514dda3e9e0b88eb3a2d61f53e6
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-23 14:53:46 +0200

    ajout champs table contacts

--- hris-vitrine/database/migrations/2025_10_23_124802_create_contacts_table.php
+++ hris-vitrine/database/migrations/2025_10_23_124802_create_contacts_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('organization')->nullable();
+            $table->string('city')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->text('message');
             $table->timestamps();
         });
     }
