158563ea2efe703a066cb6f449edb7bea115dd5c
YbrDev-web <ybenromdhane2003@gmail.com>
2025-10-23 15:10:12 +0200

    controller contact fini

--- hris-vitrine/app/Http/Controllers/ContactController.php
+++ hris-vitrine/app/Http/Controllers/ContactController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'full_name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'organization' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'message' => 'required|string|max:255',
+        ]);
+
+        Contact::create($validated);
+        
+        return back()->with('success', 'Votre message a bien été envoyé !');
+    }
 }
