131dac3ff1d50c2d58cd72cfbc8eaaed4ab6414f
YbrDev-web <ybenromdhane2003@gmail.com>
2025-11-13 16:36:01 +0100

    commentaire explicatif de la fonction permettant d'obtenir les flux rss

--- hris-vitrine/app/Http/Controllers/ArticleController.php
+++ hris-vitrine/app/Http/Controllers/ArticleController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\FeedController;
 
 class ArticleController extends Controller
 {
+    //fonction permettant d'obtenir les differents flux rss et leurs articles dans la bdd. 
     public function index()
     {
         $articles = FeedController::getArticles();
